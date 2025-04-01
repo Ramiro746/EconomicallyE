0709a2d1d5407a83b374eda5322bc685b790cb2e
Jose <jalm06@educa.madrid.org>
2025-04-01 11:20:26 +0200

    Probando commit

--- src/main/java/es/jose/economicallye/Controller/FinanceController.java
+++ src/main/java/es/jose/economicallye/Controller/FinanceController.java
@@ -32,7 +32,7 @@ public class FinanceController {
     @Autowired
     private OpenAiService openAiService;
 
-    @PostMapping("/{usuarioId}/analizar")
+    @PostMapping("/{usuarioId}/analizar")//an
     public ResponseEntity<?> analyzeFinances(@PathVariable Long userId) {
         User user = usuarioRepository.findById(userId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario no encontrado"));
