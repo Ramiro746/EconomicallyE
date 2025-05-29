c86e602a2537a16a9fd12ddc00aec76328051452
Ramiro746 <josecueva746@gmail.com>
2025-05-29 14:09:57 +0200

    Actualizando endpoints login y register y CORS en Render ahora si

--- src/main/java/es/jose/economicallye/Config/WebConfig.java
+++ src/main/java/es/jose/economicallye/Config/WebConfig.java
@@ -1,17 +1,24 @@
 package es.jose.economicallye.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;  // <-- IMPORT CORRECTO
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5174", "http://localhost:5173", "https://economically-e.vercel.app") // URL de tu frontend React
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://economically-e.vercel.app");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
